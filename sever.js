7ea53f8bf7519b0943283e2b0e84193784e2ff12
JTannut <61398316+JTannut@users.noreply.github.com>
2021-12-12 12:35:02 +0700

    first_commit

---
+++ sever.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+global.Task = require('./api/models/taskModel')
+const routes = require('./api/routes/taskRoutes')
+
+
+mongoose.connect(
+    'mongodb://localhost:27018/card_db',
+    { useNewUrlParser : true }
+)
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+routes(app);
+app.listen(port);
+
+app.use((req,res) => {
+    res.status(404).send({ url: `${req.originalUrl} non found `})
+})
+
+console.log(`Sever started on port ${port}`);
